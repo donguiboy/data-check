3029c4ac55ac7c05ce0f61e416a21246f46adf18
gonzalo lara <gonzaloflara@gmail.com>
2023-02-18 15:29:30 -0300

    finalice el primer ejercicio usando 3.14 para pi

--- demo.py
+++ demo.py
@@ -3,5 +3,7 @@
 
 
 def circle_area(radius):
-    """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    if radius > 0:
+        area = 3.14 *(radius**2)
+        return area
+    return 0
